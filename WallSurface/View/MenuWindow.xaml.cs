af4245f5f801aca67e037eb08155abf19af4ba76
Albanek203 <oleglovchuk@ukr.net>
2021-07-13 09:21:13 +0300

    Initial upload

---
+++ WallSurface/View/MenuWindow.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace WallSurface.View {
+    public partial class MenuWindow : Window {
+        public MenuWindow() { InitializeComponent(); }
+    }
+}
---
+++ WallSurface/View/ResponseExit.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace WallSurface.View {
+    public partial class ResponseExit : Window {
+        public ResponseExit() { InitializeComponent(); }
+    }
+}
